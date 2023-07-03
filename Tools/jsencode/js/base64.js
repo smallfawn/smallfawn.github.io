b4526768b8261d7916492afb7c5f42a85189a04f
smallfawn <860562056@QQ.COM>
2023-07-03 21:05:09 +0800

    add

---
+++ Tools/jsencode/js/base64.js
@@ -0,0 +1,9 @@
+// Base64加密
+function base64Encode(str) {
+  return btoa(str);
+}
+
+// Base64解密
+function base64Decode(str) {
+  return atob(str);
+}
---
+++ Tools/jsencode/js/url.js
@@ -0,0 +1,9 @@
+// URL编码
+function urlEncode(str) {
+  return encodeURIComponent(str);
+}
+
+// URL解码
+function urlDecode(str) {
+  return decodeURIComponent(str);
+}
