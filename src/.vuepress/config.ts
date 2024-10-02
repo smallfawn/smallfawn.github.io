28002c67618f3346b7ab5119a014ab86c39672ed
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-02 19:41:23 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,8 +7,7 @@ import busuanziPlugin from "@smallfawn/vuepress-plugin-busuanzi";
 import musicPlugin from "@smallfawn/vuepress-plugin-music";
 import hitokotoPlugin from "@smallfawn/vuepress-plugin-hitokoto";
 import analyticsPlugin from "@smallfawn/vuepress-plugin-analytics";
-import watermarkPlugin from "@smallfawn/vuepress-plugin-watermark";
-
+import copyrightPlugin from "@smallfawn/vuepress-plugin-copyright";
 export default defineUserConfig({
   base: "/",
   lang: "zh-CN",
@@ -20,7 +19,9 @@ export default defineUserConfig({
     musicPlugin({ src: "https://download.samplelib.com/mp3/sample-15s.mp3" }),
     hitokotoPlugin({ type: "a" }),
     analyticsPlugin({ type: "51la-v5", key: "21922161" }),
-    watermarkPlugin(),
+    copyrightPlugin({
+      copyrightText: "\n\n转载自: smallfawn 地址: https://smallfawn.github.io",
+    }),
   ],
   /*locales: {
     "/": {
