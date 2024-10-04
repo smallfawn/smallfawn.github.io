6b276749cf2922fcdd6d86e96f69c8938a01c062
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-04 20:58:57 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,8 +10,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "smallfawn",
+  description: "smallfawn",
   plugins: [
     googleAdSensePlugin({
       id: "ca-pub-2247483547544632",
