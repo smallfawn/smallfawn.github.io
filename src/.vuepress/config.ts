70bc6d83933da3772aa2184ad03c6d6c4c590378
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-07 14:04:15 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,9 +2,7 @@ import { defineUserConfig } from "vuepress";
 
 import theme from "./theme.js";
 import googleAdSensePlugin from "@smallfawn/vuepress-plugin-googleadsense";
-import busuanziPlugin from "@smallfawn/vuepress-plugin-busuanzi";
 import hitokotoPlugin from "@smallfawn/vuepress-plugin-hitokoto";
-//import analyticsPlugin from "@smallfawn/vuepress-plugin-analytics";
 import copyrightPlugin from "@smallfawn/vuepress-plugin-copyright";
 export default defineUserConfig({
   base: "/",
@@ -18,12 +16,7 @@ export default defineUserConfig({
     googleAdSensePlugin({
       id: "ca-pub-2247483547544632",
     }),
-    busuanziPlugin({
-      server: "https://busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js",
-    }),
-    //musicPlugin({ src: "https://download.samplelib.com/mp3/sample-15s.mp3" }),
     hitokotoPlugin({ type: "a" }),
-    //analyticsPlugin({ type: "51la-v5", key: "21922161" }),
     copyrightPlugin({
       copyrightText: "\n\n转载自: smallfawn 地址: https://smallfawn.github.io",
     }),
