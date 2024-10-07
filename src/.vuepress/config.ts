a618a8c7f8eb95c296750db9026c498ff7b45cee
smallfawn <101914820+smallfawn@users.noreply.github.com>
2024-10-07 13:59:54 +0800

    Update config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,7 +8,9 @@ import hitokotoPlugin from "@smallfawn/vuepress-plugin-hitokoto";
 import copyrightPlugin from "@smallfawn/vuepress-plugin-copyright";
 export default defineUserConfig({
   base: "/",
-
+  head: [
+    ["meta", { name: "google-adsense-account", content: "ca-pub-2247483547544632" }],
+  ],
   lang: "zh-CN",
   title: "smallfawn",
   description: "smallfawn",
